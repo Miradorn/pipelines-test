7c215016c5e0227f4915e86ea1a7643832075562
Alexander Steppke <alex@steppke.dev>
2022-04-08 13:07:41 +0200

    add dev and prod

--- lib/pipeline-test-stack.ts
+++ lib/pipeline-test-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps, pipelines } from 'aws-cdk-lib';
+import { Stage, StageProps, Stack, StackProps, pipelines, aws_lambda as lambda, aws_s3_deployment as s3deploy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -13,11 +13,28 @@ export class PipelineTestStack extends Stack {
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PipelineTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    pipeline.addStage(new MyPipelineAppStage(this, "Dev", {env: props?.env}))
+    pipeline.addStage(new MyPipelineAppStage(this, "Prod", {env: props?.env}))
   }
+}
+
+
+  export class MyPipelineAppStage extends Stage {
+    
+    constructor(scope: Construct, id: string, props?: StageProps) {
+      super(scope, id, props);
+  
+      const lambdaStack = new MyLambdaStack(this, 'LambdaStack');      
+    }
+}
+  export class MyLambdaStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+      super(scope, id, props);
+  
+      new lambda.Function(this, 'LambdaFunction', {
+        runtime: lambda.Runtime.NODEJS_12_X,
+        handler: 'index.handler',
+        code: new lambda.InlineCode('exports.handler = _ => "Hello, CDK";')
+      });
+    }
 }
