f836493f793f054cb41315ef65cbb98a576c5df5
Alexander Steppke <alex@steppke.dev>
2022-04-08 13:00:03 +0200

    bootstrap

--- lib/pipeline-test-stack.ts
+++ lib/pipeline-test-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, pipelines } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,6 +6,13 @@ export class PipelineTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.gitHub('miradorn/pipelines-test', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
     // The code that defines your stack goes here
 
     // example resource
